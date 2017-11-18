13644c66b71ef3199937615cf4a61df8f2bfbb5f
Lirry <lirry.pinter@student.uva.nl>
2017-11-18 15:26:08 +0100

    Testcases added

--- nlp-parsing-strings/main.py
+++ nlp-parsing-strings/main.py
@@ -1,21 +1,106 @@
 import csv
 import json
 
+import sys
+
+import globalvars
+
+
+## TODO: CREATE ENVIROMENT WITH GLOBAL VARIABLES FOR DATAPAYLOAD AND ALL THE OTHERS
+
+
+## Declaring variables, need to be put in enviroment file later
+globalvars.init()
+
+## Getting google API key from the user
+text = input("Please enter key: ")
+globalvars.test_key = (text)
+querystring = {"key": ""}
+
+
+## Putting given key in querystring for API
+for key, value in querystring.items():
+    # change the key for user input
+    querystring[key] = globalvars.test_key
+    print(querystring[key])
 
 ## Opening json containing the festivals and bios
 with open('onlybio.json', 'r') as data_file:
     festival_data = json.load(data_file)
 
+## Iterating over de keys, making sure bio is not empty
+for key in festival_data.keys():
+    key_value = festival_data[key]
+    if key_value != [{}] and key_value[0] != {}:
+        ## Check what is in the bio
+        bio = key_value[0]['bio']
+        ## Add the bio to the list
+        globalvars.bio_list.append(bio)
+        ## Parse key into string, print the festival and the bio in the csv file
+        i = str(key)
+        globalvars.festival_list.append(i)
 
 
-with open('test_festivals.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    for key in festival_data.keys():
-        key_value = festival_data[key]
-        if key_value != [{}]:
-            print(key_value)
-            i = str(key)
-            writer.writerow([i])
+
+# Length of lists
+bio_list_len = len(globalvars.bio_list)
+festival_list_len = len(globalvars.festival_list)
+
+## Making sure lists are of same len
+if bio_list_len != festival_list_len:
+    print("ERROR: Bio and festivals not same len")
+    sys.exit()
+
+
+
+## asking which bio to be used
+while True:
+    bio_input = input("Which bio you want to check?(input = number): ")
+    if not bio_input.isdigit() or not 0 < int(bio_input) < festival_list_len :
+        print("Wrong input! Which bio you want to check?: ")
+        continue
+    else:
+        print("Correct input!")
+        break
+
+globalvars.test_case = int(bio_input)
+
+
+
+## TODO, CHECK HOW I WRITE COLLUMNS FOR EACH ELEMENT
+#with open('test_festivals.csv', 'w') as csvfile:
+    #writer = csv.DictWriter(csvfile, fieldnames=['fdfdsfd', 'sfdfsd'], extrasaction='ignore')
+    #writer.writeheader()
+   # for key in csv_dict.keys():
+       #key_value = csv_dict[key]
+       # writer.writerow(key_value)
+
+
+
+
+
+## Change bio string for POST
+bio_string = globalvars.bio_list[globalvars.test_case]
+
+
+# Declaring payload for google API POST request, bio_string is parsed
+data_payload = {
+  "encodingType": "UTF8",
+  "document": {
+    "type": "PLAIN_TEXT",
+    "content": bio_string
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- nlp-parsing-strings/post_request.py
+++ nlp-parsing-strings/post_request.py
@@ -1,16 +1,26 @@
 import requests
+import main
+
+
 
 url = "https://language.googleapis.com/v1/documents:analyzeEntities"
 
-querystring = {"key":"AIzaSyDt0ASiOvNu8s_egemsX2NI7-QERNQyslA"}
+querystring = {"key": ""}
 
 
-payload = "{\r\n  \"encodingType\": \"UTF8\",\r\n  \"document\": {\r\n    \"type\": \"PLAIN_TEXT\",\r\n    \"content\":  }\r\n}"
+
+payload = "{\r\n  \"encodingType\": \"UTF8\",\r\n  \"document\": {\r\n    \"type\": \"PLAIN_TEXT\",\r\n    \"content\": \"placehodler\"\r\n  }\r\n}"
+
 headers = {
     'cache-control': "no-cache",
     'postman-token': "c8d1159f-f3ef-6ff0-0916-bb72106f079b"
-    }
+}
 
-response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+response = requests.request("POST", url, json=main.data_payload, headers=headers, params=main.querystring)
 
 print(response.text)
+
+
+##f = open('137.json', 'w')
+##f.write(response.text)
+##f.close()
